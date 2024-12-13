1d7799631633b0dbc464a409336e914c7415ea82
Холмухаммад <ksakhibov@inbox.ru>
2024-12-14 01:48:37 +0300

    Developed user profile, editing profile and password

---
+++ dashboard/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Your email',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+
 from item.models import Item
+from django.contrib.auth.models import User
+from .forms import UserForm
 
 @login_required
 def index(request):
     items = Item.objects.filter(created_by=request.user)
 
     return render(request, 'dashboard/index.html', context={'items': items})
+
+
+@login_required
+def edit(request):
+    user = request.user
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('/dashboard/')
+    else:
+        form = UserForm(instance=user)
+
+    return render(request, 'dashboard/edit.html', {'user': user, 'form': form})
+
+@login_required
+def new_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('/dashboard/')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'dashboard/new_password.html', {'form': form})
